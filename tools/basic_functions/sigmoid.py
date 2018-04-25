ee97b66955b9283f8409b51d1dc28057d674cd10
mayankkumarsingh <mayankkumarsingh@users.noreply.github.com>
2018-04-25 15:59:29 -0500

    added sigmoids: testing commit #2

---
+++ tools/basic_functions/sigmoid.py
@@ -0,0 +1,13 @@
+import numpy as np
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1/(1+np.exp(-1*x))
+    return s
---
+++ tools/basic_functions/sigmoid_gradient.py
@@ -0,0 +1,18 @@
+import sigmoid
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+
+    s = sigmoid(x)
+    ds = s*(1-s)
+
+    return ds
